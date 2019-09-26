ba20992607a1bdf7161e1510706f606a5a8ddae6
reshetnikd <dmitry.reshetnik@icloud.com>
2019-09-26 22:17:29 +0300

    Initial Commit

---
+++ EmojiArt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EmojiArt
+//
+//  Created by Dmitry Reshetnik on 9/26/19.
+//  Copyright © 2019 Dmitry Reshetnik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
